a0efb24cf95ceabaee7ab894d8c0d875b2f6fb6c
janapain <kuntz.jana@googlemail.com>
2017-09-21 01:07:05 -0700

    Router - Jana

--- app.js
+++ app.js
@@ -4,10 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
+var router = require('./router');
 var app = express();
 
 // view engine setup
@@ -21,9 +18,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', index);
-app.use('/', users);
+app.use('/', router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
