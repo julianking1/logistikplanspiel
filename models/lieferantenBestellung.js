b720b62c8cb14e65f2583d763022e1ec5a5e4388
Julian König <julian_koenig96@gmx.de>
2017-09-21 01:57:57 -0700

    Struktur

--- models/lieferantenBestellung.js
+++ models/lieferantenBestellung.js
@@ -1,9 +1,18 @@
-'use strict';
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
 
-class suppliersOrder{
-    var from = "";
-
-    constructor(){
-
+var lieferantenBestellung = new Schema({
+    von:                {type: String, required:true},
+    nr:                 {type: Number, required:true},
+    austellungstakt:    {type: Number, required:true},
+    artikel:            {type: String, required:true},
+    wunschmenge:        {type: Number, required:false},
+    wunschtakt:         {type: Number, required:false},
+    bestaetigteMenge:   {type: Number, required:false},
+    bestaetigterTakt:   {type: Number, required:false},
+    erhalteneMenge:     {type: Number, required:false},
+    erhaltenerTakt:     {type: Number, required:false},
     }
-}
+);
+
+module.exports = mongoose.model('lieferantenbestellung', lieferantenBestellung);
