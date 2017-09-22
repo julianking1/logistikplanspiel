7f7ae7b67b561343d833e1538f27c672806b360b
Julian König <julian_koenig96@gmx.de>
2017-09-22 04:24:56 -0700

    schemaFunctions

--- helpers/schemaFunctions.js
+++ helpers/schemaFunctions.js
@@ -1,19 +1,16 @@
-/*alle Schemas importieren
+//alle Schemas importieren
 var lieferantenBestellung = require ("../models/lieferantenBestellung.js");
 
-module.exports = {
-    createLieferantenBestellung: createLieferantenBestellung(),
-    getLieferantenBestellung: getLieferantenBestellung()
+function createLieferantenBestellung(von, nr, ausstellungstakt, artikel) {
+    var lieferantenBestellung = new lieferantenBestellung({von:von,
+        nr:nr, austellungstakt:ausstellungstakt, artikel:artikel});
+    lieferantenBestellung.save();
 }
-
-    function createLieferantenBestellung(von, nr, ausstellungstakt, artikel) {
-        var lieferantenBestellung = new lieferantenBestellung({von:von,
-            nr:nr, austellungstakt:ausstellungstakt, artikel:artikel});
-        lieferantenBestellung.save();
-    }
 
     function getLieferantenBestellung() {
 
     }
-*/
 
+module.exports = {
+    createLieferantenBestellung: createLieferantenBestellung
+}
