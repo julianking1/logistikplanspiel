24944d9c86721c16e4dea368f168ad144e8b8e79
lauready <lauramaria.graeber@yahoo.de>
2017-09-22 02:07:37 -0700

    Laura

---
+++ models/lagerbestandEingangsprüfung.js
@@ -0,0 +1,12 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+
+var leSchema = new Schema ({
+    spielID: {type: Number, required: true},
+    periode: {type: Number, required: true},
+    takt:    {type: Number, required: true},
+    artikel:  {type: Number, required: true}
+});
+
+module.exports = mongoose.model('lagerbestandEingangsprüfung', leSchemaSchema);
