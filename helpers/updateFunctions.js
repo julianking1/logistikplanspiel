b671623dfdffa531a1fda6ad6a79770b18a27620
Julian König <julian_koenig96@gmx.de>
2017-09-25 06:19:40 -0700

    schemaFunctions

---
+++ helpers/updateFunctions.js
@@ -0,0 +1,41 @@
+var getterFunctions = require("../helpers/getterFunctions");
+
+//alle Schemas importieren
+var lieferantenBestellung = require ("../models/lieferantenBestellung.js");
+// KEINE OPTIONALEN PARAMETER  var beschaffungsBestandswert = require("../models/beschaffungBestandswert");
+// KEINE OPTIONALEN PARAMETER  var beschaffungBestelluebersicht = require("../models/beschaffungBestelluebersicht");
+var beschaffungKPI = require("../models/beschaffungKPI");
+// KEINE OPTIONALEN PARAMETER  var beschaffungsUebersicht = require("../models/beschaffungsUebersicht");
+var endmontageFertigungsplan = require("../models/endmontageFertigungsplan");
+var endmontageKPI = require("../models/endmontageKPI");
+// KEINE OPTIONALEN PARAMETER  var endmontageLagerbestand = require("../models/endmontageLagerbestand");
+// KEINE OPTIONALEN PARAMETER  var endmontageLagersumme = require("../models/endmontageLagersumme");
+// KEINE OPTIONALEN PARAMETER  var endmontageLagerzugang = require("../models/endmontageLagerzugang");
+// KEINE OPTIONALEN PARAMETER  var endmontageLosesumme = require("../models/endmontageLosesumme");
+// KEINE OPTIONALEN PARAMETER  var lagerbestandEingangspruefung = require("../models/lagerbestandEingangspruefung");
+// KEINE OPTIONALEN PARAMETER  var lagerbestandvorEingangspruefung = require("../models/lagerbestandvorEingangspruefung");
+var vorfertigungFertigungsplan = require("../models/vorfertigungFertigungsplan");
+var vorfertigungKPI = require("../models/vorfertigungKPI");
+// KEINE OPTIONALEN PARAMETER  var vorfertigungLagerbestand = require("../models/vorfertigungLagerbestand");
+// KEINE OPTIONALEN PARAMETER  var vorfertigungLagersumme = require("../models/vorfertigungLagersumme");
+// KEINE OPTIONALEN PARAMETER  var vorfertigungLagerzugang = require("../models/vorfertigungLagerzugang");
+// KEINE OPTIONALEN PARAMETER  var vorfertigungLosesumme = require("../models/vorfertigungLosesumme");
+// KEINE OPTIONALEN PARAMETER  var distributionAuftragsbearbeitung= require("../models/distributionAuftragsbearbeitung");
+var distributionKPI = require("../models/distributionKPI");
+// KEINE OPTIONALEN PARAMETER  var distributionLagerbestand= require("../models/distributionLagerbestand");
+var distributionSummen= require("../models/distributionSummen");
+// KEINE OPTIONALEN PARAMETER  var distributionLagerzugang =require("../models/distributionLagerzugang");
+var kundeAuftragsuebersicht= require("../models/kundeAuftragsuebersicht");
+var kundeKPI= require("../models/kundeKPI");
+var kundeUmsatz= require("../models/kundeUmsatz");
+var lieferabwicklungBestelluebersicht= require("../models/lieferabwicklungBestelluebersicht");
+var lieferabwicklungKPI= require("../models/lieferabwicklungKPI");
+// KEINE OPTIONALEN PARAMETER  var lieferabwicklungLagerbestand= require("../models/lieferabwicklungLagerbestand");
+// KEINE OPTIONALEN PARAMETER  var lieferabwicklungLagerzugang= require("../models/lieferabwicklungLagerzugang");
+// KEINE OPTIONALEN PARAMETER  var lieferabwicklungSummen= require("../models/lieferabwicklungSummen");
+var orderManagement= require("../models/orderManagement");
+
+
+function update() {
+
+}
