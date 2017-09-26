28b49a2b0676eb979c27ddf62abbd87987c90d6d
lauready <lauramaria.graeber@yahoo.de>
2017-09-26 04:59:19 -0700

    Laura

--- helpers/getterFunctions.js
+++ helpers/getterFunctions.js
@@ -76,6 +76,44 @@ function getbeschaffungsUebersicht(spielID, periode, callback) {
 
 };
 
+function getdistributionAuftragsbearbeitung(spielID, periode, takt, callback) {
+    distributionAuftragsbearbeitung.findOne({'spielID':spielID, 'periode': periode, 'takt':takt}, function (err, data) {
+        if(err) return handleError (err);
+        return callback(data);
+    });
+
+};
+
+function getdistributionKPI(spielID, periode, callback){
+    distributionKPI.findOne({'spielID': spielID, 'periode': periode}, function(err, data){
+        if(err) return handleError (err);
+        return callback(data);
+    });
+};
+
+function getdistributionLagerbestand(spielID, periode, takt, artikel, callback){
+    distributionLagerbestand.findOne({'spielID': spielID, 'periode': periode,'takt':takt, 'artikel':artikel}, function(err, data){
+        if(err) return handleError (err);
+        return callback(data);
+    });
+};
+
+function getdistributionLagerzugang(spielID, periode, takt, artikel, callback){
+    distributionLagerzugang.findOne({'spielID': spielID, 'periode': periode,'takt':takt, 'artikel':artikel}, function(err, data){
+        if(err) return handleError (err);
+        return callback(data);
+    });
+};
+
+function getdistributionSummen(spielID, periode, callback){
+    distributionSummen.findOne({'spielID': spielID, 'periode': periode}, function(err, data){
+        if(err) return handleError (err);
+        return callback(data);
+    });
+};
+
+
+
 
 
 // liefert den letzten Bestand aus der Eingangsprüfung zurück
@@ -188,6 +226,10 @@ function getvorfertigungLosesumme(spielID, periode, artikel, callback) {
 
 
 module.exports = {
+    getdistributionAuftragsbearbeitung:getdistributionAuftragsbearbeitung,
+    getdistributionKPI:getdistributionKPI,
+    getdistributionLagerbestand:getdistributionLagerbestand,
+    getdistributionLagerzugang:getdistributionLagerzugang,
     getalllieferabwicklungLagerzugang:getalllieferabwicklungLagerzugang,
     getlagerbestandEingangspruefung: getlagerbestandEingangspruefung,
     getlagerbestandvorEingangspruefung: getlagerbestandvorEingangspruefung,
